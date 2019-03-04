25f78c5d0914e495c9d4c6e16dfa5b706d955d44
NikKovIos <nikkovios@gmail.com>
2019-03-04 23:19:27 +0300

    Added weak self to prevent crashing.

--- LGAlertView/LGAlertViewController.m
+++ LGAlertView/LGAlertViewController.m
@@ -53,11 +53,13 @@
 
 - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
     [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-
+    
+    __weak typeof(self) weakSelf = self;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [UIView animateWithDuration:coordinator.transitionDuration animations:^{
-            [self setNeedsStatusBarAppearanceUpdate];
-            [self.alertView layoutValidateWithSize:size];
+            __typeof__(self) strongSelf = weakSelf;
+            [strongSelf setNeedsStatusBarAppearanceUpdate];
+            [strongSelf.alertView layoutValidateWithSize:size];
         }];
     });
 }
