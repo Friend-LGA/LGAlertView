251a0312121956b12c5e4c16b495cb1bb08e85a1
NikeKov <nikkovios@gmail.com>
2024-09-06 11:28:19 +0300

    Fixed crash in viewWillTransitionToSize

--- LGAlertView/LGAlertViewController.m
+++ LGAlertView/LGAlertViewController.m
@@ -52,14 +52,29 @@
 }
 
 - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
-    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    if (coordinator) {
+        [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    }
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [UIView animateWithDuration:coordinator.transitionDuration animations:^{
-            [self setNeedsStatusBarAppearanceUpdate];
-            [self.alertView layoutValidateWithSize:size];
+    @try {
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            __weak typeof(self) weakSelf = self;
+            [UIView animateWithDuration:0.5 animations:^{
+                if (weakSelf) {
+                    if ([weakSelf respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)]) {
+                        [weakSelf setNeedsStatusBarAppearanceUpdate];
+                    }
+                    if (weakSelf.alertView) {
+                        [weakSelf.alertView layoutValidateWithSize:size];
+                    }
+                }
+            }];
         }];
-    });
+    } @catch (NSException *exception) {
+        //
+    } @finally {
+        //
+    }
 }
 
 #pragma mark -
