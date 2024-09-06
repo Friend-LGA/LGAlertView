096b256d4f0a51c0a6c9e8f7792b455cc197e26a
Grigorii Lutkov <Friend.LGA@gmail.com>
2024-09-07 00:16:59 +0300

    Update demo project (#91)
    * build: migrate to a new xcode version
    
    * feat: add safe area inset and fix navigation bar style
    
    * build: unify code style

--- Demo/LGAlertViewDemo/NavigationController.m
+++ Demo/LGAlertViewDemo/NavigationController.m
@@ -14,6 +14,15 @@
     self.navigationBar.barTintColor = [UIColor colorWithRed:0.0 green:0.5 blue:1.0 alpha:1.0];
     self.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
     self.navigationBar.tintColor = [UIColor colorWithWhite:1.0 alpha:0.5];
+
+    if (@available(iOS 13.0, *)) {
+        UINavigationBarAppearance *standardAppearance = [UINavigationBarAppearance new];
+        standardAppearance.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
+        standardAppearance.backgroundColor = [UIColor colorWithRed:0.0 green:0.5 blue:1.0 alpha:1.0];
+        self.navigationBar.standardAppearance = standardAppearance;
+        self.navigationBar.scrollEdgeAppearance = standardAppearance;
+        self.navigationBar.compactAppearance = standardAppearance;
+    }
 }
 
 - (BOOL)shouldAutorotate {
